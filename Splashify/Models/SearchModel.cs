1872eca6dffc5d9c8de1a01b6cd90fb8a1d2cb11
Anton <antonhelgerosbo@gmail.com>
2021-05-03 14:11:43 +0200

    start of Dashboard Search

---
+++ Splashify/Models/SearchModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Splashify.Models
+{
+    
+    public class SearchModel
+    {
+        public string SearchField { get; set; }
+        public int value { get; set; }
+
+        public string name { get; set; }
+        public string startdate { get; set; }
+        public string gender { get; set; }
+    }
+
+}
